ffd7915d0c3826245cb5f9be932b92721ba4330b
Akshit joshi <akshitcasual1@gmail.com>
2026-03-03 22:11:17 +0000

    2

--- nodes/advocate.py
+++ nodes/advocate.py
@@ -1,8 +1,9 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from state import state
-
-model = ChatGoogleGenerativeAI(model="gemini-2.0-flash", google_api_key="AIzaSyAngNwyHN2x_CsTX3BRtrP8U6_blBm4TII")
+from dotenv import load_dotenv
+import os
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash", google_api_key=os.getenv("GOOGLE_API_KEY"))
 
 prompt = ChatPromptTemplate.from_messages([
     ("system", "You are a strong advocate for DAO proposals. Given a proposal, build the strongest possible case in favor of it. Be persuasive, concise, and focus on community benefit, growth, and value."),
