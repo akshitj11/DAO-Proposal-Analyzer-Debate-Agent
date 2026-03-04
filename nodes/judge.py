e9c062f611cab5f23b160c3ea941d7c48525d514
Akshit joshi <akshitcasual1@gmail.com>
2026-03-04 12:40:22 +0000

    done

--- nodes/judge.py
+++ nodes/judge.py
@@ -1,6 +1,6 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
-from state import state
+from state import State
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -16,8 +16,19 @@ Confidence: <number out of 10>"""),
     ("human", "Advocate argues: {advocate_argument}\nCritic argues: {critic_argument}")
 ])
 
+chain = prompt | model
 def judge_node(state:State):
     response = chain.invoke({
-        "advocate_arugument":state["advocate_argument"],
-        "critic_argument":state{"critic_argument"}
-    })
+        "advocate_argument":state["advocate_argument"],
+        "critic_argument":state["critic_argument"]})
+      #parsing the output
+
+    lines = response.content.strip().split("\n")
+    verdict = lines[0].replace("Verdict:","").strip()
+    confidence = int(lines[1].replace("Confidence","").strip())
+
+    return {
+    "verdict":verdict,
+    "confidence":confidence,
+    "round":state["round"]+1
+     }
